f01dda9882f61d6fe82daecda0938de0aa81e808
user1 <canord2010@yandex.ru>
2025-10-07 20:07:13 +0300

    Base

---
+++ file1.php
@@ -0,0 +1,26 @@
+<?php
+
+function GetText1(): string
+{
+    return "Text1";
+}
+
+function GetText2(): string
+{
+    return "Text2";
+}
+
+function GetText22(): string
+{
+    return "Text2";
+}
+
+function GetText3(): string
+{
+    return "Text3";
+}
+
+function GetText4(): string
+{
+    return "Text3";
+}
